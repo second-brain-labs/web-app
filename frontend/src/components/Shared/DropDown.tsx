55dcdf4ed58af38f6811b5a511f4e63c0337c693
rickyraup <73044129+rickyraup@users.noreply.github.com>
2023-11-18 23:15:26 -0500

    Rraup/frontend/dashboard (#17)
    * some styling fixes
    
    * some frontend updates

--- frontend/src/components/Shared/DropDown.tsx
+++ frontend/src/components/Shared/DropDown.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Stack, Typography } from '@mui/material';
 
 
 
 const DropDown = () => {
-    return (
-        <h1>My Spaces</h1>
+
+    const [open, setOpen] = useState(false);
+    return (<>
+        {!open &&
+        <Stack onClick={()=>setOpen(true)}>
+            <Typography>My Spaces</Typography>
+        </Stack>
+        }
+        {open &&
+            <>
+            <Stack onClick={()=>setOpen(false)}>
+                <Typography>My Spaces</Typography>
+            </Stack>
+            <li>
+                <ul>Space 1</ul>
+                <ul>Space 2</ul>
+            </li>
+
+            </>
+        }
+        </>
+        
     );
 }
 
