4864252a7a5ee583e8badd3cb56247340366a41a
Vikram Singh <vsingh18567@gmail.com>
2023-11-23 15:56:24 -0800

    Auth & Chrome Extension Integration (#18)
    * this should work
    
    * we are a go for chrome extension functionality

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -5,31 +5,35 @@ from app.schemas.users import UserSchema, UserCreateSchema
 
 router = APIRouter(prefix="/users")
 
-@router.get("/{user_uuid}", response_model=UserSchema)
-async def get_user(user_uuid: int, db=Depends(get_db)):
 
-    user : UserModel = db.query(UserModel).filter(UserModel.uuid == user_uuid).first()
+@router.get("/me/{user_uuid}", response_model=UserSchema)
+async def get_user(user_uuid: str, db=Depends(get_db)):
+    user: UserModel = db.query(UserModel).filter(UserModel.uuid == user_uuid).first()
 
     return user
 
-@router.post("/dummy/create/", response_model=UserSchema)
-async def create_dummy_user(user: UserCreateSchema, db=Depends(get_db)):
+
+@router.post("/login", response_model=UserSchema)
+async def create_user_return_existing(user: UserCreateSchema, db=Depends(get_db)):
     # check if user with email exists
-    existing_user : UserModel = db.query(UserModel).filter(UserModel.email == user.email).first()
+    existing_user: UserModel = (
+        db.query(UserModel).filter(UserModel.email == user.email).first()
+    )
     if existing_user:
         return existing_user
-    user : UserModel = UserModel(name=user.name, email=user.email, password=user.password)
+    user: UserModel = UserModel(name=user.name, email=user.email)
     db.add(user)
     db.commit()
     db.refresh(user)
-    directory : DirectoryModel = DirectoryModel(name="/", user_uuid=user.uuid)
+    directory: DirectoryModel = DirectoryModel(name="/", user_uuid=user.uuid)
     db.add(directory)
     db.commit()
 
     return user
 
+
 @router.get("/dummy/all")
 async def dummy_get_users(db=Depends(get_db)):
     # DUMMY ROUTE
-    users : list[UserModel] = db.query(UserModel).all()
+    users: list[UserModel] = db.query(UserModel).all()
     return users
--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,18 +1,22 @@
 from pydantic import BaseModel
 from app.schemas.articles import ArticleSchema
 
+
 class UserBaseSchema(BaseModel):
     email: str
     name: str
 
+
 class UserCreateSchema(UserBaseSchema):
-    password: str
+    pass
+
 
 class UserSchema(UserBaseSchema):
     uuid: str
 
     class Config:
         from_attributes = True
+
 
 class UserArticlesSchema(UserSchema):
     articles: list[ArticleSchema] = []
