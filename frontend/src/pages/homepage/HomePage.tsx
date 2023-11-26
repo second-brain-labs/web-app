b7721279babcb49b640b571b5494fdd73cb8a9e9
vsingh18567 <vsingh18567@gmail.com>
2023-11-25 19:04:43 -0800

    this shd fix urls

--- frontend/src/pages/homepage/HomePage.tsx
+++ frontend/src/pages/homepage/HomePage.tsx
@@ -16,12 +16,12 @@ const HomePage = () => {
     const [user, setUser] = useState("");
 
     const getUsername = async (userID: string) => {
-        const response = axios.get(`http://localhost:3500/users/${userID}`);
+        const response = axios.get(`http://localhost:3500/users/me/${userID}`);
         const data = (await response).data;
         setUser(data.name);
     }
     const userID = "e0db2e0b-e3eb-484e-b96d-edaf9ae04fc2";
-    useEffect(()=>{
+    useEffect(() => {
         getUsername(userID);
     }, []);
 
@@ -39,34 +39,34 @@ const HomePage = () => {
 
     return (
         <Stack direction={"row"}
-        sx={{width: "100%", height: "100%", margin: "10px"}}
+            sx={{ width: "100%", height: "100%", margin: "10px" }}
         >
-            <Stack sx={{width: "30%", height: "100%"}}>
-                <Logo/>
-                <DropDown/>
-                <DropDown/>
-                <Chat/>
+            <Stack sx={{ width: "30%", height: "100%" }}>
+                <Logo />
+                <DropDown />
+                <DropDown />
+                <Chat />
             </Stack>
 
-            <Stack sx={{width: "90%", height: "100%"}}>
+            <Stack sx={{ width: "90%", height: "100%" }}>
                 <Stack
-                sx={{width: "90%", height: "100%"}}
-                direction={"row"}
-                justifyContent={"space-between"}
+                    sx={{ width: "90%", height: "100%" }}
+                    direction={"row"}
+                    justifyContent={"space-between"}
                 >
                     <Stack
                         direction={"row"}
-                        sx={{width: "100%", height: "100%"}}
-                        >
+                        sx={{ width: "100%", height: "100%" }}
+                    >
                         <Typography>Space Title</Typography>
                     </Stack>
                     <Stack
                         direction={"row"}
-                        >
+                    >
                         <Typography>{user}</Typography>
                     </Stack>
                 </Stack>
-                <FileView user_uuid={userID}/>
+                <FileView user_uuid={userID} />
             </Stack>
 
         </Stack>
