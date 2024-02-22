3cba63afc4728933225705d570cca18567055920
Rohan Gupta <rohangupta883@gmail.com>
2024-02-21 21:17:57 -0500

    production runscrpt

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-if os.getenv("APP_ENV").lower() == "production":
+if os.getenv("APP_ENV", "").lower() == "production":
     POSTGRES_HOST = os.getenv("POSTGRES_HOST_PROD")
     POSTGRES_PORT = os.getenv("POSTGRES_PORT_PROD")
     POSTGRES_USER = os.getenv("POSTGRES_USER_PROD")
@@ -14,7 +14,7 @@ if os.getenv("APP_ENV").lower() == "production":
     SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/postgres"
 else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ build_and_publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# This shell script builds docker images and drops them into the k3s container
+# TODO: automatically update k8s manifest image versions
+#
+set -euxo pipefail
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version_string>"
+    exit 1
+fi
+
+version=$1
+
+docker build -t frontend:$version ./frontend
+ocker build -t backend:$version ./backend
+docker save frontend:$version | k3s ctr images import -
+docker save backend:$version | k3s ctr images import -
